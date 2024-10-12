d932eb746b29cb209848bf1a06a25f20612962c0
Ami Gohel <amigohel303@gmail.com>
2024-10-12 22:44:08 +0530

    Prerequisites Angular CLI TypeScript Tailwind CSS Approach Set up a new Angular project. Integrate Tailwind CSS for styling. Create and configure necessary components. Implement routing for navigation. Style components using Tailwind CSS.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutComponent } from "./about/about.component";
+import { HomeComponent } from "./home/home.component";
+import { ProjectsComponent } from "./projects/projects.component";
+import { ContactComponent } from "./contact/contact.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AboutComponent, HomeComponent, ProjectsComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
